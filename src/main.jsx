d19a16b6fc312730ca8b80462d8e2d7176708da7
Abanoub-Abd-Elmessih <abanoubabdelmessih110@gmail.com>
2024-10-26 15:34:58 +0300

    Fix Navbar error

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    
     <App />
   </React.StrictMode>,
 )
